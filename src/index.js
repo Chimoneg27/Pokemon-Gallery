dbfbb8f2325532dad3d4e2c43f4c198674e1995c
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-06 12:50:54 +0200

    Add code for card popups

--- src/index.js
+++ src/index.js
@@ -2,13 +2,38 @@ import './style.css';
 import getCardTypes from './modules/getCardTypes.js';
 
 const cardBoxes = document.querySelector('.card-boxes');
-
 // getCardImages
 const pokemonImage = (pokeID, container) => {
   const image = document.createElement('img');
   // imageID
   image.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeID}.png`;
   container.append(image);
+};
+
+const openPopup = (pokeData) => {
+  const modalBox = document.querySelector('.modal');
+  modalBox.innerHTML = `
+      <div class="popup">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeData.id}.png" alt="${pokeData.name}" />
+          <h3>${pokeData.name}</h3>
+          <p>#${pokeData.id}</p>
+          <button class="closePopupBtn">Close</button>
+      </div>
+  `;
+  const popup = document.querySelector('.popup');
+  const pokeType = document.createElement('ul');
+  pokeType.innerHTML = 'Type(s)';
+  pokeType.classList.add('types');
+  popup.appendChild(pokeType);
+
+  getCardTypes(pokeData.types, pokeType);
+
+  const closePopupBtn = modalBox.querySelector('.closePopupBtn');
+  closePopupBtn.addEventListener('click', () => {
+    modalBox.style.display = 'none';
+  });
+
+  modalBox.style.display = 'flex';
 };
 
 // renderFetchedData
@@ -21,17 +46,16 @@ const renderPokemonData = (pokeData) => {
   pokemonName.innerText = pokeData.name;
   pokeCard.appendChild(pokemonName);
 
-  //   const moves = shorterArray(pokeData.moves);
-  const pokeType = document.createElement('ul');
-  pokeCard.appendChild(pokeType);
-
-  getCardTypes(pokeData.types, pokeType);
-
   pokeCard.innerHTML += `
-      <p>#${pokeData.id}</p>
+      <button class='popupBtn'>Comment</button>
       `;
 
   cardBoxes.appendChild(pokeCard);
+
+  const popupBtn = pokeCard.querySelector('.popupBtn');
+  popupBtn.addEventListener('click', () => {
+    openPopup(pokeData);
+  });
 };
 
 // getMoreCardInfo;
