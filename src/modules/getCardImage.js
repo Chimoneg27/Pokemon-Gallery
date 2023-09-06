1820c1ab7bbab696e9fe15e896d1b5299135f130
unknown <ismayito1@gmail.com>
2023-09-06 01:18:50 -0700

    Removed getImages  module

--- src/modules/getCardImage.js
+++
@@ -1,9 +0,0 @@
-// function to fetch images of the data
-const getCardImages = (imageId, container) => {
-  console.log(imageId);
-  const image = document.createElement('img');
-  image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${imageId}.png`;
-  container.append(image);
-};
-
-export default getCardImages;
