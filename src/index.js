56a6422c606d54c37c176bab1cacf709ce08a4fa
unknown <ismayito1@gmail.com>
2023-09-06 02:10:22 -0700

    Fixed linter errors and styled the home page

--- src/index.js
+++ src/index.js
@@ -2,65 +2,66 @@ import './style.css';
 import getCardTypes from './modules/getCardTypes.js';
 import shorterArray from './modules/shorterArray.js';
 
-let cardBoxes = document.querySelector('.card-boxes');
-
-const getPokemon = async () => {
-    const sourceUrl = "https://pokeapi.co/api/v2/pokemon?limit=6"
-    try {
-        const response = await fetch(sourceUrl)
-        const data = await response.json();
-        const pokeArray = data.results;
-        pokeArray.forEach((pokemon) => {
-            getMoreInfo(pokemon);
-        })
-    } catch (error) {
-        console.error("Error fetching data:", error)
-    }
-}
-// getMoreCardInfo;
-const getMoreInfo = (pokemon) => {
-    const sourceUrl = pokemon.url;
-    fetch(sourceUrl)
-        .then(response => response.json())
-        .then((pokeData) => {
-            renderPokemonData(pokeData);
-        })
-        .catch((error) => {
-            console.error("Error fetching more info:", error);
-        });
-}
-
-// renderFetchedData
-const renderPokemonData = (pokeData) => {
-    let pokeCard = document.createElement('div');
-    pokeCard.classList.add('poke-dex');
-    // console.log(pokeData.moves);
-    pokemonImage(pokeData.id, pokeCard);
-
-    let pokemonName = document.createElement('h2');
-    pokemonName.innerText = pokeData.name;
-    pokeCard.appendChild(pokemonName);
-
-    let moves = shorterArray(pokeData.moves);
-    console.log(moves)
-    let pokeType = document.createElement('ul');
-    pokeCard.appendChild(pokeType);
-
-    getCardTypes(pokeData.types, pokeType)
-
-    pokeCard.innerHTML += `
-    <p>#${pokeData.id}</p>
-    `;
-
-    cardBoxes.appendChild(pokeCard);
-}
+const cardBoxes = document.querySelector('.card-boxes');
 
 // getCardImages
 const pokemonImage = (pokeID, container) => {
-    let image = document.createElement('img');
-                                                                                            //imageID
-    image.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeID}.png`
-    container.append(image);
-}
+  const image = document.createElement('img');
+  // imageID
+  image.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeID}.png`;
+  container.append(image);
+};
+
+// renderFetchedData
+const renderPokemonData = (pokeData) => {
+  const pokeCard = document.createElement('div');
+  pokeCard.classList.add('poke-dex');
+  // console.log(pokeData.moves);
+  pokemonImage(pokeData.id, pokeCard);
+
+  const pokemonName = document.createElement('h2');
+  pokemonName.innerText = pokeData.name;
+  pokeCard.appendChild(pokemonName);
+
+  const moves = shorterArray(pokeData.moves);
+  console.log(moves);
+  const pokeType = document.createElement('ul');
+  pokeCard.appendChild(pokeType);
+
+  getCardTypes(pokeData.types, pokeType);
+
+  pokeCard.innerHTML += `
+      <p>#${pokeData.id}</p>
+      `;
+
+  cardBoxes.appendChild(pokeCard);
+};
+
+// getMoreCardInfo;
+const getMoreInfo = (pokemon) => {
+  const sourceUrl = pokemon.url;
+  fetch(sourceUrl)
+    .then((response) => response.json())
+    .then((pokeData) => {
+      renderPokemonData(pokeData);
+    })
+    .catch((error) => {
+      console.error('Error fetching more info:', error);
+    });
+};
+
+const getPokemon = async () => {
+  const sourceUrl = 'https://pokeapi.co/api/v2/pokemon?limit=6';
+  try {
+    const response = await fetch(sourceUrl);
+    const data = await response.json();
+    const pokeArray = data.results;
+    pokeArray.forEach((pokemon) => {
+      getMoreInfo(pokemon);
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
 
 getPokemon();
--- src/modules/shorterArray.js
+++ src/modules/shorterArray.js
@@ -1,6 +1,6 @@
-const shorterArray=(moveSet)=>{
-    let shortArr = moveSet.slice(0, 3);
-    return shortArr;
-}
+const shorterArray = (moveSet) => {
+  const shortArr = moveSet.slice(0, 3);
+  return shortArr;
+};
 
 export default shorterArray;
