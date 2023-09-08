d88796b13308b5bcf3379232a80158680379a90c
unknown <ismayito1@gmail.com>
2023-09-08 06:16:07 -0700

    Added the fetch and post likes function

---
+++ src/modules/postLikes.js
@@ -0,0 +1,19 @@
+const postLikes = async (data) => {
+  const appId = 'qNEqt1NyUkKxEIK764gq';
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes/`;
+  try {
+    const postData = await fetch(`${url}`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json; charset=UTF-8' },
+      body: JSON.stringify(data),
+
+    });
+    const response = await postData.json();
+    return response.ok;
+  } catch (error) {
+    console.error('Error:', error);
+    return error.message;
+  }
+};
+
+export default postLikes;
